e1e7d8490e1fed99a61b922cf90e82212205b29b
Elizelton Lisboa Dos Santos <elizelton@DESKTOP-1VB78HG.localdomain>
2020-12-27 19:52:00 -0300

    :building_construction: config ambiente

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://localhost:8080/api',
+  apiURLAuth: 'https://localhost:8080/auth'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'https://localhost:4200/api',
+  apiURLAuth: 'https://localhost:4200/auth'
 };
 
 /*
